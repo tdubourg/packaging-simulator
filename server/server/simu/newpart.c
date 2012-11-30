b3171997f574df484bb1d95e07a762484d9a8323
Troll <trolldu26@gmail.com>
2012-11-30 11:52:08 +0100

    Adding folder to put simulation related files

---
+++ server/server/simu/newpart.c
@@ -0,0 +1,5 @@
+
+void newpart(void* a) {
+	
+	
+}
---
+++ server/server/simu/newpart.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   newpart.h
+ * Author: troll
+ *
+ * Created on November 30, 2012, 11:50 AM
+ */
+
+#ifndef NEWPART_H
+#define	NEWPART_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+	void newpart(void*);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* NEWPART_H */
+
