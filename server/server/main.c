8607f490c7ba7b5f29407e5f318f93bca3778b77
Gautier DI FOLCO <gautier.difolco@gmail.com>
2012-11-30 11:56:36 +0100

    Premier sem.

--- server/server/main.c
+++ server/server/main.c
@@ -7,12 +7,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <pthread.h>
+#include <semaphore.h>
+sem_t semCtrlBox;
 /*
  * 
  */
 int main(int argc, char** argv) {
 
+    sem_init(&semCtrlBox, 0, 1);
     printf("Hello, world\n");
     return (EXIT_SUCCESS);
 }
