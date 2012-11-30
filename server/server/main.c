c9ce18253f00edf812e2a488fe9558028ba15d85
Gautier DI FOLCO <gautier.difolco@gmail.com>
2012-11-30 11:40:18 +0100

    Création du projet netbeans server + .gitignore.

---
+++ server/server/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: black
+ *
+ * Created on November 30, 2012, 11:32 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello, world\n");
+    return (EXIT_SUCCESS);
+}
+
