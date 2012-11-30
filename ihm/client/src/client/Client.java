c4e96dda4c22a3de673edb04ebbebfb1773eb9d7
Tatata <pierre@localhost.localdomain>
2012-11-30 10:54:36 +0100

    Ajout du client

---
+++ ihm/client/src/client/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author pierre
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World !!!");
+    }
+}
