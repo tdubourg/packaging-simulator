af41f4f6254531c258b11f7b00b6fee310c81eae
Troll <trolldu26@gmail.com>
2012-12-01 16:16:19 +0100

    code in the right file

--- server/server/partsPackager.c
+++ server/server/partsPackager.c
@@ -1,3 +1,27 @@
 void partsPackager(void*) {
-    
+	extern int PARTS_BY_BOX;
+	extern semt_t* semSyncBoxImp;
+	
+	int currentBoxPartsNumber = 0;
+	//**** INIT
+	
+
+
+	//**** MAIN LOOP
+	for (;;) {
+		//**** SYNC SEMAPHORE CHECK
+
+		//* There's a new part to put in that freaking box:
+		currentBoxPartsNumber = (currentBoxPartsNumber + 1) % PARTS_BY_BOX;
+
+		if (!currentBoxPartsNumber) //* Is the box full?
+		{
+			//**** "READY TO GO TO PRINTER" SEMAPHORE CHECK
+			sem_wait(semSyncBoxImp);
+		}
+
+	}
+
+
+	/**** END / CLEANING 
 }
