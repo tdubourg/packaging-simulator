a3d8f5f19bd9ef3f3161d51afbda7e4a2038c43f
Ubuntu <cedric@ubuntu.(none)>
2012-12-04 15:18:11 +0100

    Writting logs in file (tested)

--- server/server/doLog.c
+++ server/server/doLog.c
@@ -2,39 +2,55 @@
 #include "common.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 void *doLog(void *p) {
-    
+
     char buffer[MAX_MSG_LEN + 1];
     unsigned int sender;
     int bytes_read;
     bool keepRunning = TRUE;
+    time_t temps;
+    struct tm date;
 
 
     //opening mbox
-     mqd_t mboxLogs = mq_open(MBOXLOGS, O_RDWR);
-    
-    //opening file
-    FILE *f = fopen(LOG_FILE_NAME, "wb");
-    
-    while(keepRunning) {
-        
+    mqd_t mboxLogs = mq_open(MBOXLOGS, O_RDWR);
+
+    while (keepRunning) {
         bytes_read = mq_receive(mboxLogs, buffer, MAX_MSG_LEN, NULL);
-        printf("%d",bytes_read);
+        //printf("%d", bytes_read);
         if (bytes_read == -1) {
-            perror("[MainThread] Failed to recieve");
+            perror("[LogThread] Failed to recieve");
             //return 0;
-        }
-        else {
-            printf("[MainThread] Data: %s \n", buffer);
-            
+        } else {
+            printf("[LogThread] Data: %s %d\n", buffer, bytes_read);
+            //debug
+
+
+
+
+            time(&temps);
+            date = *localtime(&temps);
+
+
+
+            //TODO : keep file and close it once thread end is handled
+            FILE *f = fopen(LOG_FILE_NAME, "a+");
             //writting in logg file
-            fwrite(buffer, bytes_read, 1, f);
-            
-            memset(buffer, 0, MAX_MSG_LEN); 
-            
+            fprintf(f, "[%02d/%02d/%d|%02d:%02d:%02d] %s\n",
+                    date.tm_mday + 1,
+                    date.tm_mon + 1,
+                    date.tm_year + 1900,
+                    date.tm_hour,
+                    date.tm_min,
+                    date.tm_sec,
+                    buffer);
+            fclose(f);
+
+
+            memset(buffer, 0, bytes_read + 1);
         }
     }
-    fclose(f);
 
 }
