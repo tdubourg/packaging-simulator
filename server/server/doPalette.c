tdubourg/packaging-simulator

commit 24f72ddee088c0ce3291a3f94d3eb6760903ad6e

Suppresion de paletteCond, CondPalette existait ! Ajout de la macro INCLUDE pour inclure une variable condition.

server/server/doPalette.c
@@ -4,8 +4,7 @@
 void *doPalette(void *p)
 {
 	extern int BOXES_BY_PALETTE;
-	extern pthread_mutex_t paletteLock;
-	extern pthread_cond_t paletteCond;
+	INCLUDE(Palette)
 	extern int BOXES_QUEUE;
 	extern sem_t SemSyncImpPalette;
     int currentPaletteBoxesNumber = 0;
@@ -16,15 +15,15 @@ void *doPalette(void *p)
 	
     for(;;) {
 		sem_wait(&SemSyncImpPalette);
-		pthread_mutex_lock(&paletteLock);
+		pthread_mutex_lock(&LockPalette);
 		while (BOXES_QUEUE <= 0) { /* We're paused */
-			pthread_cond_wait(&paletteCond, &paletteLock); /* Wait for play signal */
+			pthread_cond_wait(&CondPalette, &LockPalette); /* Wait for play signal */
 		}
 		DBG("doPalette", "Main", "New box added in palette");
         --BOXES_QUEUE;
 		currentPaletteBoxesNumber = (currentPaletteBoxesNumber + 1) % BOXES_BY_PALETTE;
-		pthread_cond_signal(&paletteCond);
-		pthread_mutex_unlock(&paletteLock);
+		pthread_cond_signal(&CondPalette);
+		pthread_mutex_unlock(&LockPalette);
 		
         sem_post(&SemSyncImpPalette);
 	}

server/server/doPrint.c
@@ -6,23 +6,22 @@
 void *doPrint(void *p) {
 	extern sem_t SemSyncBoxImp;
 	extern sem_t SemPushBoxImp;
-	extern pthread_mutex_t paletteLock;
-	extern pthread_cond_t paletteCond;
+	INCLUDE(Palette)
 	extern int BOXES_QUEUE;
 	extern int MAX_BOXES_QUEUE;
 
 	for(;;) {
 		sem_wait(&SemPushBoxImp);
 		
-		pthread_mutex_lock(&paletteLock);
+		pthread_mutex_lock(&LockPalette);
 		while (BOXES_QUEUE >= MAX_BOXES_QUEUE) { /* We're paused */
-			pthread_cond_wait(&paletteCond, &paletteLock); /* Wait for play signal */
+			pthread_cond_wait(&CondPalette, &LockPalette); /* Wait for play signal */
 		}
 		DBG("doPrint", "Main", "Printing");
-        BOXES_QUEUE++;
-		pthread_cond_signal(&paletteCond);
+        ++BOXES_QUEUE;
+		pthread_cond_signal(&CondPalette);
 		DBG("doPrint", "Main", "New box in the queue");
-		pthread_mutex_unlock(&paletteLock);
+		pthread_mutex_unlock(&LockPalette);
 		
 		sem_post(&SemSyncBoxImp);
 	}

server/server/main.c
@@ -23,9 +23,6 @@ bool LockImpValue;
 bool LockPaletteValue;
 bool LockValveValue;
 
-pthread_mutex_t paletteLock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t paletteCond = PTHREAD_COND_INITIALIZER;
-
 int STOCKS = 0;
 int PARTS_BY_BOX = 10;
 int BOXES_BY_PALETTE = 10;

server/server/partsPackager.c
@@ -10,8 +10,6 @@
 #include "time.h"
 
 static bool simu_refusal() {
-	extern pthread_mutex_t LockBox;
-	extern pthread_cond_t CondBox;
 	static bool init = FALSE;
 
 	if (!init) {
@@ -32,9 +30,7 @@ void* partsPackager(void*a) {
 	extern sem_t SemSyncBoxImp;
 	extern sem_t SemPushBoxImp;
 	extern sem_t SemNewPart;
-	extern pthread_mutex_t LockBox;
-	extern pthread_cond_t CondBox;
-	extern bool LockBoxValue;
+	INCLUDE(Box)
 	int refusedPartsCount = 0; //* Number of parts that have been refused for the current box (not to be higher than MAX_REFUSED_PARTS_BY_BOX)
 	int currentBoxPartsNumber = 0;
 	//**** INIT

server/server/common.h
@@ -56,5 +56,10 @@ typedef unsigned char bool;
        pthread_cond_signal(&Cond ## V);\
        pthread_mutex_unlock(&Lock ## V);
 
+/* Include a variable condition*/
+#define INCLUDE(V) extern pthread_mutex_t Lock ## V;\
+	extern pthread_cond_t Cond ## V;\
+	extern bool Lock ## V ## Value;
+
 #endif	/* COMMON_H */
 

server/server/doControl.c
@@ -9,20 +9,10 @@
  */
 void *doControl(void *p)
 {
-	extern pthread_mutex_t LockImp;
-	extern pthread_mutex_t LockPalette;
-	extern pthread_mutex_t LockValve;
-	extern pthread_mutex_t LockBox;
-	
-	extern pthread_cond_t CondValve;
-	extern pthread_cond_t CondBox;
-	extern pthread_cond_t CondPalette;
-	extern pthread_cond_t CondImp;
-	
-	extern bool LockBoxValue;
-	extern bool LockImpValue;
-	extern bool LockPaletteValue;
-	extern bool LockValveValue;
+	INCLUDE(Imp)
+	INCLUDE(Palette)
+	INCLUDE(Valve)
+	INCLUDE(Box)
 
 	char msg[MAX_MSG_LEN+1];
 	mqd_t mboxControl = mq_open(MBOXCONTROL, O_RDWR);

commit c276f8b4a663f2ba4dc47c04f4e9f21b23a44554

Fixing errors introduced by commit 24f72ddee088c0ce3291a3f94d3eb6760903ad6e (deleting paletteCond, believe it was a duplicate where it is not)

server/server/doPalette.c
@@ -3,9 +3,11 @@
 
 void *doPalette(void *p)
 {
-	extern int BOXES_BY_PALETTE;
 	INCLUDE(Palette)
-	extern int BOXES_QUEUE;
+	extern int BOXES_BY_PALETTE;
+	extern pthread_mutex_t paletteLock;
+	extern pthread_cond_t paletteCond;
+	extern int BoxesQueue;
 	extern sem_t SemSyncImpPalette;
     int currentPaletteBoxesNumber = 0;
 	
@@ -15,15 +17,15 @@ void *doPalette(void *p)
 	
     for(;;) {
 		sem_wait(&SemSyncImpPalette);
-		pthread_mutex_lock(&LockPalette);
-		while (BOXES_QUEUE <= 0) { /* We're paused */
-			pthread_cond_wait(&CondPalette, &LockPalette); /* Wait for play signal */
+		pthread_mutex_lock(&paletteLock);
+		while (BoxesQueue <= 0) { /* We're paused */
+			pthread_cond_wait(&paletteCond, &paletteLock); /* Wait for play signal */
 		}
 		DBG("doPalette", "Main", "New box added in palette");
-        --BOXES_QUEUE;
+        --BoxesQueue;
 		currentPaletteBoxesNumber = (currentPaletteBoxesNumber + 1) % BOXES_BY_PALETTE;
-		pthread_cond_signal(&CondPalette);
-		pthread_mutex_unlock(&LockPalette);
+		pthread_cond_signal(&paletteCond);
+		pthread_mutex_unlock(&paletteLock);
 		
         sem_post(&SemSyncImpPalette);
 	}

server/server/doPrint.c
@@ -4,24 +4,26 @@
 #include <stdio.h>
 
 void *doPrint(void *p) {
+	INCLUDE(Print)
 	extern sem_t SemSyncBoxImp;
 	extern sem_t SemPushBoxImp;
-	INCLUDE(Palette)
-	extern int BOXES_QUEUE;
+	extern pthread_mutex_t paletteLock;
+	extern pthread_cond_t paletteCond;
+	extern int BoxesQueue;
 	extern int MAX_BOXES_QUEUE;
 
 	for(;;) {
 		sem_wait(&SemPushBoxImp);
 		
-		pthread_mutex_lock(&LockPalette);
-		while (BOXES_QUEUE >= MAX_BOXES_QUEUE) { /* We're paused */
-			pthread_cond_wait(&CondPalette, &LockPalette); /* Wait for play signal */
+		pthread_mutex_lock(&paletteLock);
+		while (BoxesQueue >= MAX_BOXES_QUEUE) { /* We're paused */
+			pthread_cond_wait(&paletteCond, &paletteLock); /* Wait for play signal */
 		}
 		DBG("doPrint", "Main", "Printing");
-        ++BOXES_QUEUE;
-		pthread_cond_signal(&CondPalette);
+        BoxesQueue++;
+		pthread_cond_signal(&paletteCond);
 		DBG("doPrint", "Main", "New box in the queue");
-		pthread_mutex_unlock(&LockPalette);
+		pthread_mutex_unlock(&paletteLock);
 		
 		sem_post(&SemSyncBoxImp);
 	}

server/server/main.c
@@ -13,11 +13,14 @@ pthread_mutex_t LockBox = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t LockImp = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t LockPalette = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t LockValve = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t paletteLock;
 
 pthread_cond_t CondValve = PTHREAD_COND_INITIALIZER;
 pthread_cond_t CondBox = PTHREAD_COND_INITIALIZER;
 pthread_cond_t CondPalette = PTHREAD_COND_INITIALIZER;
 pthread_cond_t CondImp = PTHREAD_COND_INITIALIZER;
+pthread_cond_t paletteCond;
+
 bool LockBoxValue;
 bool LockImpValue;
 bool LockPaletteValue;
@@ -27,8 +30,9 @@ int STOCKS = 0;
 int PARTS_BY_BOX = 10;
 int BOXES_BY_PALETTE = 10;
 int MAX_REFUSED_PARTS_BY_BOX = 42;
-int BOXES_QUEUE = 0;
+int BoxesQueue = 0;
 int MAX_BOXES_QUEUE = 10;
+
 
 bool needToStop = TRUE;
 

server/server/partsPackager.c
@@ -86,12 +86,9 @@ void* partsPackager(void*a) {
 
 							//* @TODO : replace message with correct format
 				//* Closing the valve
-				pthread_mutex_lock(&LockValve);
+				SET(Valve, FALSE);
 				DBG("doControl", "Main", "Closing valve.");
-				LockValveValue = FALSE;
-				pthread_cond_signal(&CondValve);
-				pthread_mutex_unlock(&LockValve);
-
+				
 				// Sending error message (priority 2)
 				int res=mq_send(mboxControl, "E Error refused parts rate", MAX_MSG_LEN, 2);
 				refusedPartsCount = 0;
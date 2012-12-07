c276f8b4a663f2ba4dc47c04f4e9f21b23a44554
Troll <trolldu26@gmail.com>
2012-12-07 18:16:44 +0100

    Fixing errors introduced by commit 24f72ddee088c0ce3291a3f94d3eb6760903ad6e (deleting paletteCond, believe it was a duplicate where it is not)

--- server/server/doPrint.c
+++ server/server/doPrint.c
@@ -4,24 +4,26 @@
 #include <stdio.h>
 
 void *doPrint(void *p) {
+	INCLUDE(Print)
 	extern sem_t SemSyncBoxImp;
 	extern sem_t SemPushBoxImp;
-	INCLUDE(Palette)
-	extern int BOXES_QUEUE;
+	extern pthread_mutex_t paletteLock;
+	extern pthread_cond_t paletteCond;
+	extern int BoxesQueue;
 	extern int MAX_BOXES_QUEUE;
 
 	for(;;) {
 		sem_wait(&SemPushBoxImp);
 		
-		pthread_mutex_lock(&LockPalette);
-		while (BOXES_QUEUE >= MAX_BOXES_QUEUE) { /* We're paused */
-			pthread_cond_wait(&CondPalette, &LockPalette); /* Wait for play signal */
+		pthread_mutex_lock(&paletteLock);
+		while (BoxesQueue >= MAX_BOXES_QUEUE) { /* We're paused */
+			pthread_cond_wait(&paletteCond, &paletteLock); /* Wait for play signal */
 		}
 		DBG("doPrint", "Main", "Printing");
-        ++BOXES_QUEUE;
-		pthread_cond_signal(&CondPalette);
+        BoxesQueue++;
+		pthread_cond_signal(&paletteCond);
 		DBG("doPrint", "Main", "New box in the queue");
-		pthread_mutex_unlock(&LockPalette);
+		pthread_mutex_unlock(&paletteLock);
 		
 		sem_post(&SemSyncBoxImp);
 	}
