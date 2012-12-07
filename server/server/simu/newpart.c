9c5e1853f43e8e04edb0ef84fb545979a48381da
Troll <trolldu26@gmail.com>
2012-12-07 17:57:22 +0100

    Now stopping pushing new parts when valve is closed. Better debug message too.

--- server/server/simu/newpart.c
+++ server/server/simu/newpart.c
@@ -9,10 +9,19 @@
 sem_t SemNewPart;
 
 void *newpart(void* a) {
+	extern pthread_cond_t CondValve;
+	extern pthread_mutex_t LockValve;
+	extern bool LockValveValue;
+
 	for(;;) {
-		printf("Pouet\n");
-		usleep(500 * 1000);
+		pthread_mutex_lock(&LockValve);
+		while(!LockValveValue) { /* We're paused */
+			pthread_cond_wait(&CondValve, &LockValve); /* Wait for play signal */
+		}
+		pthread_mutex_unlock(&LockValve);
+		DBG("newpart", "Main", "Pushing new part");
 		sem_post(&SemNewPart);
+		usleep(500 * 1000);
 	}
 }
 
