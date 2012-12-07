74f92ebfba3eff38c47145ee80cc8f3c40555390
Troll <trolldu26@gmail.com>
2012-12-07 08:59:19 +0100

    Tested the fact we can lock AND UNLOCK the partsPackager task. Also switching from spaces to tabs

--- server/server/main.c
+++ server/server/main.c
@@ -5,6 +5,7 @@
 sem_t SemCtrlBox;
 sem_t SemCtrlPallet;
 sem_t SemCtrlImp;
+
 sem_t SemSyncBoxImp;
 sem_t SemPushBoxImp;
 sem_t SemSyncImpPalette;
@@ -33,89 +34,95 @@ int MAX_REFUSED_PARTS_BY_BOX = 42;
 #endif
 
 int main(int argc, char** argv) {
-    pthread_t tBox, tCommunication, tControl, tLog, tPalette, tPrint, tWarehouse;
-    mqd_t mboxCommunication, mboxControl, mboxLogs, mboxPalletStore;
+	pthread_t tBox, tCommunication, tControl, tLog, tPalette, tPrint, tWarehouse;
+	mqd_t mboxCommunication, mboxControl, mboxLogs, mboxPalletStore;
 #ifdef SIMU_MODE
-    mqd_t tSimuNewPart;
+	mqd_t tSimuNewPart;
 #endif
 
-    sem_init(&SemCtrlBox, 0, 1);
+	sem_init(&SemCtrlBox, 0, 1);
 
-    // Temporary stuff, to be renamed
-    //--------------
-    
-    pthread_mutex_lock(&boxLock);
-    boxLockBool = TRUE;
-    pthread_cond_signal(&boxCond);
-    pthread_mutex_unlock(&boxLock);
-    //--------------
+	// Temporary stuff, to be renamed
+	//--------------
+	
+	pthread_mutex_lock(&boxLock);
+	boxLockBool = TRUE;
+	pthread_cond_signal(&boxCond);
+	pthread_mutex_unlock(&boxLock);
+	//--------------
 
-    sem_init(&SemCtrlPallet, 0, 1);
-    sem_init(&SemCtrlImp, 0, 1);
-    sem_init(&SemSyncBoxImp, 0, 1);
-    sem_init(&SemPushBoxImp, 0, 0);
-    sem_init(&SemSyncImpPalette, 0, 1);
-    sem_init(&SemSocket, 0, 1);
-    sem_init(&SemStock, 0, 1);
+	sem_init(&SemCtrlPallet, 0, 1);
+	sem_init(&SemCtrlImp, 0, 1);
+	sem_init(&SemSyncBoxImp, 0, 1);
+	sem_init(&SemPushBoxImp, 0, 0);
+	sem_init(&SemSyncImpPalette, 0, 1);
+	sem_init(&SemSocket, 0, 1);
+	sem_init(&SemStock, 0, 1);
 
 
 #ifdef SIMU_MODE
-    extern sem_t SemNewPart;
-    sem_init(&SemNewPart, 0, 0);
+	extern sem_t SemNewPart;
+	sem_init(&SemNewPart, 0, 0);
 #endif
 	// Open message queues
-    mboxCommunication = mq_open(MBOXCOMMUNICATION, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
-    mboxControl = mq_open(MBOXCONTROL, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
-    mboxLogs = mq_open(MBOXLOGS, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
-    mboxPalletStore = mq_open(MBOXPALLETSTORE, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
+	mboxCommunication = mq_open(MBOXCOMMUNICATION, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
+	mboxControl = mq_open(MBOXCONTROL, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
+	mboxLogs = mq_open(MBOXLOGS, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
+	mboxPalletStore = mq_open(MBOXPALLETSTORE, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
 
-    //wait
-    
-    pthread_create(&tLog, NULL, doLog, NULL);
-    pthread_create(&tControl, NULL, doControl, NULL);
-    pthread_create(&tWarehouse, NULL, doWarehouse, NULL);
-    pthread_create(&tPalette, NULL, doPalette, NULL);
-    pthread_create(&tPrint, NULL, doPrint, NULL);
-    pthread_create(&tBox, NULL, partsPackager, NULL);
-    pthread_create(&tCommunication, NULL, doCommunication, NULL);
+	//wait
+	
+	pthread_create(&tLog, NULL, doLog, NULL);
+	pthread_create(&tControl, NULL, doControl, NULL);
+	pthread_create(&tWarehouse, NULL, doWarehouse, NULL);
+	pthread_create(&tPalette, NULL, doPalette, NULL);
+	pthread_create(&tPrint, NULL, doPrint, NULL);
+	pthread_create(&tBox, NULL, partsPackager, NULL);
+	pthread_create(&tCommunication, NULL, doCommunication, NULL);
 #ifdef SIMU_MODE
-    pthread_create(&tSimuNewPart, NULL, newpart, NULL);
+	pthread_create(&tSimuNewPart, NULL, newpart, NULL);
 #endif
 
-    // Wait
-    //@TODO : Remove those lines that are used for testing purposes
-    // usleep(15 * 1000 * 1000);
-    // DBG("main", "Main", "======= NOW LOCKING THE partsPackager task =======");
-    // pthread_mutex_lock(&boxLock);
-    // boxLockBool = FALSE;
-    // pthread_cond_signal(&boxCond);
-    // pthread_mutex_unlock(&boxLock);
-    
+	// Wait
+	//@TODO : Remove those lines that are used for testing purposes
+	// usleep(15 * 1000 * 1000);
+	// DBG("main", "Main", "======= NOW LOCKING THE partsPackager task =======");
+	// pthread_mutex_lock(&boxLock);
+	// boxLockBool = FALSE;
+	// pthread_cond_signal(&boxCond);
+	// pthread_mutex_unlock(&boxLock);
+	// usleep(7 * 1000 * 1000);
+	// DBG("main", "Main", "======= NOW UUUUUNNNNNLOCKING THE partsPackager task =======");
+	// pthread_mutex_lock(&boxLock);
+	// boxLockBool = TRUE;
+	// pthread_cond_signal(&boxCond);
+	// pthread_mutex_unlock(&boxLock);
+	
 	// Wait for end of threads
-    pthread_join(tCommunication, NULL);
-    pthread_join(tBox, NULL);
-    pthread_join(tPrint, NULL);
-    pthread_join(tPalette, NULL);
-    pthread_join(tWarehouse, NULL);
-    pthread_join(tControl, NULL);
-    pthread_join(tLog, NULL);
+	pthread_join(tCommunication, NULL);
+	pthread_join(tBox, NULL);
+	pthread_join(tPrint, NULL);
+	pthread_join(tPalette, NULL);
+	pthread_join(tWarehouse, NULL);
+	pthread_join(tControl, NULL);
+	pthread_join(tLog, NULL);
 
-    // Deleting message queue
-    mq_close(mboxCommunication);
-    mq_close(mboxControl);
-    mq_close(mboxLogs);
-    mq_close(mboxPalletStore);
+	// Deleting message queue
+	mq_close(mboxCommunication);
+	mq_close(mboxControl);
+	mq_close(mboxLogs);
+	mq_close(mboxPalletStore);
 
-    // Deleting sems
-    sem_destroy(&SemStock);
-    sem_destroy(&SemSocket);
-    sem_destroy(&SemSyncImpPalette);
-    sem_destroy(&SemSyncBoxImp);
-    sem_destroy(&SemCtrlImp);
-    sem_destroy(&SemCtrlPallet);
-    sem_destroy(&SemCtrlBox);
+	// Deleting sems
+	sem_destroy(&SemStock);
+	sem_destroy(&SemSocket);
+	sem_destroy(&SemSyncImpPalette);
+	sem_destroy(&SemSyncBoxImp);
+	sem_destroy(&SemCtrlImp);
+	sem_destroy(&SemCtrlPallet);
+	sem_destroy(&SemCtrlBox);
 
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
