abc556c967532f3ece7f998d0d77d8290a5be3ce
Troll <trolldu26@gmail.com>
2012-12-11 23:50:31 +0100

    Using brand new macro

--- server/server/simu/newpart.c
+++ server/server/simu/newpart.c
@@ -7,11 +7,7 @@ void *newpart(void* a) {
 	INCLUDE(Valve)
 
 	for(;;) {
-		pthread_mutex_lock(&LockValve);
-		while(!LockValveValue) { /* We're paused */
-			pthread_cond_wait(&CondValve, &LockValve); /* Wait for play signal */
-		}
-		pthread_mutex_unlock(&LockValve);
+		CHECK_WAIT_BOOL(Valve);
 		DBG("newpart", "Main", "Pushing new part");
 		sem_post(&SemNewPart);
 		usleep(500 * 1000);
